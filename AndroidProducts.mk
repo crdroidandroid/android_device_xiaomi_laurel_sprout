cd16a8f4392deba2e39fde9a36d32fda06cf0c7d
Musical143143 <basub4615@gmail.com>
2022-06-20 17:33:32 +0200

    laurel_sprout: RiceDroidOs Bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/cherish_laurel_sprout.mk
+    $(LOCAL_DIR)/lineage_laurel_sprout.mk
 
 COMMON_LUNCH_CHOICES := \
-    cherish_laurel_sprout-user \
-    cherish_laurel_sprout-userdebug \
-    cherish_laurel_sprout-eng
+    lineage_laurel_sprout-user \
+    lineage_laurel_sprout-userdebug \
+    lineage_laurel_sprout-eng
--- lineage_laurel_sprout.mk
+++ lineage_laurel_sprout.mk
@@ -19,20 +19,18 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
-# Inherit some common CherishOs stuff
-$(call inherit-product, vendor/cherish/config/common_full_phone.mk)
+# Inherit some common RiceDroidOs stuff
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-# Cherish Official
-CHERISH_BUILD_TYPE := OFFICIAL
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    ro.cherish.maintainer=RAYAN(BASUBHAJANTRI)
-
-EXTRA_UDFPS_ANIMATIONS := true
+# UDFPS ICONS/ANIMATIONS
+TARGET_HAS_UDFPS := true
 HAS_FOD := true
+
+# maintainer flag
+RICE_MAINTAINER := BASUBHAJANTRI
 
 # FaceUnlock
 TARGET_FACE_UNLOCK_SUPPORTED := true
-
 
 # Pixel charging animation
 TARGET_INCLUDE_PIXEL_CHARGER := true
@@ -47,9 +45,8 @@ $(call inherit-product, $(LOCAL_PATH)/laurel_sprout.mk)
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := laurel_sprout
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := cherish_laurel_sprout
+PRODUCT_NAME := lineage_laurel_sprout
 PRODUCT_MODEL := Mi A3
-
 
 # ABI Checks
 SKIP_ABI_CHECKS := true
@@ -63,7 +60,10 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 # Gapps build
-WITH_GMS := true
+WITH_GAPPS := true
 
 # Quick tap
 TARGET_SUPPORTS_QUICK_TAP := true
+
+# Enable blur support
+TARGET_ENABLE_BLUR := true
