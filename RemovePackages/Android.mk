cb6a4e60d7aa6ef26ea6b3dfb97073fc238717ca
Musical143143 <basub4615@gmail.com>
2022-07-06 20:27:10 +0200

    laurel_sprout: Remove more packages

--- RemovePackages/Android.mk
+++ RemovePackages/Android.mk
@@ -9,6 +9,38 @@ LOCAL_OVERRIDES_PACKAGES += MyVerizonServices Music
 LOCAL_OVERRIDES_PACKAGES += OBDM_Permissions OPScreenRecord RecorderPrebuilt
 LOCAL_OVERRIDES_PACKAGES += SafetyHubPrebuilt SprintDM SprintHM
 LOCAL_OVERRIDES_PACKAGES += VzwOmaTrigger YouTube YouTubeMusicPrebuilt
+LOCAL_OVERRIDES_PACKAGES := \
+	AmbientSensePrebuilt \
+	AppDirectedSMSService \
+	CalendarGooglePrebuilt \
+	Camera2 \
+	CarrierSetup \
+	ConnMO \
+	DCMO \
+	DMService \
+	DevicePolicyPrebuilt \
+	DiagnosticsToolPrebuilt \
+	Drive \
+	HelpRtcPrebuilt \
+	Maps \
+	MyVerizonServices \
+	NgaResources \
+	OBDM_Permissions \
+	OemDmTrigger \
+	PixelLiveWallpaperPrebuilt \
+	PrebuiltGmail \
+	RecorderPrebuilt \
+	SafetyHubPrebuilt \
+	ScribePrebuilt \
+	Showcase \
+	SoundAmplifierPrebuilt \
+	SprintDM \
+	SprintHM \
+	Tycho \
+	USCCDM \
+	VZWAPNLib \
+	VzwOmaTriger \
+
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
