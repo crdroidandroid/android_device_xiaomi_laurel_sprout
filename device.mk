bbf93c29e31ae4f1734ecc98cdf5414a77e4bea2
Arindam Bhattacharjee <abhattacharjee717@gmail.com>
2024-08-01 10:47:25 +0000

    laurel_sprout: Add `Dolby Atmos`
    repo - https://github.com/userariii/vendor_motorola-dolby.git
    
    Signed-off-by: Arindam Bhattacharjee <abhattacharjee717@gmail.com>

--- device.mk
+++ device.mk
@@ -67,6 +67,9 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
+    
+# Audio (Dolby Atmos)
+$(call inherit-product, vendor/motorola-dolby/setup.mk)    
 
 # Bluetooth
 PRODUCT_PACKAGES += \
